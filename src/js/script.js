e7a606a8d3552096cac505cfccb7d1cfcaf83315
Igor Rekhtman <ihor.wave@gmail.com>
2023-01-11 23:36:16 +0200

    js commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1500,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true,      
+            }
+        }
+        ]
+        
+      });
+  });
