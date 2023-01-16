b5c82fe2282cb96474a1d25aece5412a1a4faebf
Igor Rekhtman <ihor.wave@gmail.com>
2023-01-17 01:48:16 +0200

    changes

--- src/js/script.js
+++ src/js/script.js
@@ -85,4 +85,24 @@ $(document).ready(function(){
 
       $('input[name=phone]').mask("+38 (999) 9999999");
 
+      $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+      });
+
   });
