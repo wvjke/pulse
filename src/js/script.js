e09cd9d75c36f833423ad6eb855747eda1bfe198
Igor Rekhtman <ihor.wave@gmail.com>
2023-01-17 21:16:10 +0200

    changes

--- src/js/script.js
+++ src/js/script.js
@@ -105,4 +105,40 @@ $(document).ready(function(){
         return false;
       });
 
+
+      // PAge up , SMooth scroll
+
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+      });
+
+       // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+
+    new WOW().init();
+
   });
